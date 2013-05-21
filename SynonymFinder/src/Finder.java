5c54f533caf8065104d506d93ea4ef7abcef2e51
Daniel <Daniel@Work-PC>
2013-05-21 18:56:44 +0900

    - CSV parsing initialization completed. - Naver dictionary address input completed
    TODO:
    - analyze Naver HTML page to find synonyms
    - input synonyms into DictionaryWord
    - output DictionaryWords to CSV

--- SynonymFinder/src/Finder.java
+++ SynonymFinder/src/Finder.java
@@ -4,8 +4,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Set;
 
 import au.com.bytecode.opencsv.CSVReader;
@@ -15,27 +17,46 @@ import com.gargoylesoftware.htmlunit.html.*;
 
 public class Finder {
 
-	public static void main(String args[]) throws IOException {
-		String csvFileName = "º¸°í¼­_´Ü¾î Á¤¸®_±èÁø¼º.csv";
+	public static void main(String args[]) throws Exception{
+		//getNaverData();
+		processCSV();
+	}
+	
+	public static void processCSV() throws IOException {
+		String csvFileName = "ë³´ê³ ì„œ_ë‹¨ì–´ ì •ë¦¬_ê¹€ì§„ì„±.csv";
 		File csvFile = new File(csvFileName);
 		//http://stackoverflow.com/questions/5965195/utf-8-cjk-characters-not-displaying-in-java
 		//http://viralpatel.net/blogs/java-read-write-csv-file/
 		CSVReader csvReader = new CSVReader(new FileReader(csvFileName));
 		String[] row = null;
-		while((row = csvReader.readNext()) != null) {
+		List<DictionaryWord> dictionary = new ArrayList<DictionaryWord>();
+		while((row = csvReader.readNext()) != null){
+			DictionaryWord tempWord = new Finder.DictionaryWord(row[0], row[1], row[2], row[3], row[4]);
+			dictionary.add(tempWord);
+			System.out.println(row[0]
+					+ "\n1: " + row[1]
+					+ "\n2: " + row[2]
+					+ "\n3: " + row[3]
+					+ "\n4: " + row[4]); }
+		
+		//if((row = csvReader.readNext()) != null){
+		//	System.out.println(row[0]
+	    //  		+ "\n4: " + row[4]); }
+		
+		/* while((row = csvReader.readNext()) != null) {
 		    System.out.println(row[0]
 		              + " # " + row[1]
 		              + " #  " + row[2]);
-		}
+		} //*/
 		//...
 		csvReader.close();
 	}
 	
-	public void getNaverData() throws Exception {
+	public static void lookupWord(String word) throws Exception {
 		final WebClient webClient = new WebClient();
 
 		// Get the first page
-		final HtmlPage page1 = webClient.getPage("http://some_url");
+		final HtmlPage page1 = webClient.getPage("http://krdic.naver.com/search.nhn?kind=keyword&query=" + word);
 
 		// Get the form that we are dealing with and within that form, 
 		// find the submit button and the field that we want to change.
@@ -53,6 +74,22 @@ public class Finder {
 		webClient.closeAllWindows();
 	}
 
+	private static class DictionaryWord{
+		public String koreanWord;
+		public String englishWord;
+		public String level;
+		public String connector;
+		public String synonym;
+		
+		public DictionaryWord(String koreanWord, String englishWord, String level, String connector, String synonym){
+			this.koreanWord = koreanWord;
+			this.englishWord = englishWord;
+			this.level = level;
+			this.connector = connector;
+			this.synonym = synonym;
+		}
+	}
+	
 	private class NaverWord {
 		String koreanWord;
 		String koreanDefinition;
